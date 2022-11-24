e909105a816c2ddc7e5fefda2c38b857e511885d
Quanganhhh <quanganhbui2703@gmail.com>
2022-11-24 08:50:11 +0700

    chuong4

---
+++ src/main/java/com/example/ontapsql/OntapSQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ontapsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntapSQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntapSQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ontapsql/OntapSQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ontapsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntapSQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
